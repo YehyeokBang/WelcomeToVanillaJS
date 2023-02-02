a03d07961c90fc4db02a5632273068710121b426
YehyeokBang <qkddpgur318@gmail.com>
2023-02-02 22:29:54 +0900

    feat: Add #4.7 Login Recap

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+// 아래처럼 반복되는 String들은 대문자 변수로 지정하고 사용하면 오타 찾기가 수월해짐
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 /* submit 된다고 해서 그냥 실행되는 것이 아님. 실제로는 어떠한 argument를
    넣어주어야 실행됨. 그 argument에 정체는 바로 event에 대한 기본 정보들
@@ -17,9 +19,15 @@ function onLoginSubmit(event) {
   /* loginForm은 css를 이용해 없애고 greeting을 보이게 함 */
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  // localStorage에 key-value 형태로 저장 가능
-  localStorage.setItem("username", username);
-  greeting.innerText = "Hello " + username; // `Hello ${username}`(기호 조심) 같은 방법임
+  // localStorage에 key-value 형태로 저장 가능, 유저 정보 저장
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+// <h1 id=greeting></h1> 태그 보여주는 함수
+function paintGreetings(username) {
+  // "Hello " + username과 `Hello ${username}`(기호 조심) 같은 방법임
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -27,3 +35,17 @@ function onLoginSubmit(event) {
    js는 onLoginSubmit()을 호출할 때 인자를 담아서 호출함
    해당 인자는 event object를 담은 정보들 */
 loginForm.addEventListener("submit", onLoginSubmit);
+
+/* localStorage에서 Key 값이 "username"인 Value가 있는지 확인
+   있으면 값을 가져오고, 없으면 null을 리턴함 <- localStorage.getItem()
+*/
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// 유저 정보(savedUsername)가 없으면(초기 상태 포함) 로그인 폼을 보이게 하며 submit을 기다림
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // 유저 정보가 있으면 그 정보를 기반으로 paintGreetings() 실행
+  paintGreetings(savedUsername);
+}
